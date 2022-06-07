0410824abd217c56f48ca4a7476125040b29cd4c
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-07 12:31:14 +0100

    Created conditional background class based on user assessment. question is its own unique variable in for flexiblity when question needs to change

--- src/components/QuestionPage.tsx
+++ src/components/QuestionPage.tsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import {
+  assessmentLibrary,
+  giveAnswerAssessment,
+} from "../utils/giveAnswerAssessment";
 import { StateAction, StateInterface } from "../utils/QuestionStateManager";
 import { Toggle } from "./Toggle";
 
@@ -8,25 +12,38 @@ interface QuestionProps {
 }
 
 export function QuestionPage({ state, dispatch }: QuestionProps): JSX.Element {
-  const questionOptions = state.questions[0].options;
-  const questionAnswers = state.questions[0].answers;
+  const thisQuestion = state.questions[0];
+  const questionOptions = thisQuestion.options;
+  const questionAnswers = thisQuestion.answers;
 
   const initialSelectedAnswers = new Array(questionOptions.length).fill(""); //For flexibility when number of options changes
   const [selectedAnswers, setSelectedAnswers] = useState<string[]>(
     initialSelectedAnswers
   );
 
-  const [backgroundStyle, setBackgroundStyle] = useState<string>(
-    "backgroundIncorrect"
-  );
-
-  const answerAssessment = selectedAnswers.map(
+  const markedAnswers: boolean[] = selectedAnswers.map(
     (selectedAnswer, answerIndex) =>
       selectedAnswer === questionAnswers[answerIndex]
   );
-  console.log({ a: selectedAnswers[2], b: questionAnswers[2] });
-  console.log(selectedAnswers[2] === questionAnswers[2]);
-  console.log({ actualAnswers: questionAnswers, assessment: answerAssessment });
+
+  const answerAssessment = giveAnswerAssessment(
+    markedAnswers,
+    assessmentLibrary
+  );
+
+  let backgroundStyle = "backgroundIncorrect";
+
+  if (answerAssessment === assessmentLibrary.CORRECT) {
+    backgroundStyle = "backgroundCorrect";
+  } else if (answerAssessment === assessmentLibrary.ALMOST_THERE) {
+    backgroundStyle = "backgroundAlmostThere";
+  } else if (answerAssessment === assessmentLibrary.GETTING_BETTER) {
+    backgroundStyle = "backgroundGettingBetter";
+  } else if (answerAssessment === assessmentLibrary.INCORRECT) {
+    backgroundStyle = "backgroundIncorrect";
+  }
+
+  console.log({ actualAnswers: questionAnswers, assessment: markedAnswers });
 
   const questionToggles: JSX.Element[] = questionOptions.map(
     (option, index) => (
@@ -41,11 +58,11 @@ export function QuestionPage({ state, dispatch }: QuestionProps): JSX.Element {
   );
 
   return (
-    <main className="backgroundIncorrect pageSize">
+    <main className={`${backgroundStyle} pageSize`}>
       <div>""</div>
       <section className="mt-5">
         <h1 className="text-center defaultFont questionText mb-5">
-          {state.questions[0].question}
+          {thisQuestion.question}
         </h1>
         <section className="container mx-auto mb-5">{questionToggles}</section>
         <h2 className="text-center defaultFont resultText">
