e27a7513a122f90d81187111fdaa89d2f541fc0b
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-07 20:36:52 +0100

    Stlyed home-page. Added button which links to question. Removed fix width from pageSize css

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function HomePage(): JSX.Element {
   return (
-    <>
-      <button>Hello</button>
-    </>
+    <main className="d-flex backgroundCorrect pageSize">
+      <div className="m-auto">
+        <Link to="/1">
+          <button className="defaultFont btn btn-success btn-lg">
+            <h1>Begin Learning</h1>
+          </button>
+        </Link>
+      </div>
+    </main>
   );
 }
