6b27c5f26d153a82fc2d44cb5b27b277d99e562a
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-07 15:01:31 +0100

    Removed initialInterface to declare in Question page as it needs information, Creted unique payloadInterface to have access to question answers. Reassignment and selection and answer marking happen together in reducer

--- src/utils/QuestionStateManager.ts
+++ src/utils/QuestionStateManager.ts
@@ -1,22 +1,26 @@
+import {
+  assessmentLibrary,
+  giveAnswerAssessment,
+} from "./giveAnswerAssessment";
 
 export const stateActionsLibrary = {
-  SET_ANSWERS: "SET_ANSWERS",
+  SET_SELECTED_ANSWERS_AND_MARK: "SET_SELECTED_ANSWERS_AND_MARK",
   SET_LOCK: "SET_LOCK",
 };
 
 export interface StateInterface {
   selectedAnswers: string[];
+  answerAssessment: string;
   is_locked: boolean;
 }
 
-export const initialState: StateInterface = {
-  selectedAnswers: [],
-  is_locked: false,
-};
+interface PayloadInterface extends StateInterface {
+  actualAnswers: string[];
+}
 
 export interface StateAction {
   type: string;
-  payload: StateInterface;
+  payload: PayloadInterface;
 }
 
 export function reducer(
@@ -24,8 +28,27 @@ export function reducer(
   action: StateAction
 ): StateInterface {
   switch (action.type) {
-    case stateActionsLibrary.SET_ANSWERS: {
-      return { ...state, selectedAnswers: action.payload.selectedAnswers };
+    case stateActionsLibrary.SET_SELECTED_ANSWERS_AND_MARK: {
+      //SET The new selected answers
+      //mark the answer, create an answer assessment
+      //SET that to in state.
+      const newSelectedAnswers = action.payload.selectedAnswers;
+      const questionAnswers = action.payload.actualAnswers;
+      const markedAnswers: boolean[] = newSelectedAnswers.map(
+        (selectedAnswer, answerIndex) =>
+          selectedAnswer === questionAnswers[answerIndex]
+      );
+
+      const answerAssessment = giveAnswerAssessment(
+        markedAnswers,
+        assessmentLibrary
+      );
+
+      return {
+        ...state,
+        selectedAnswers: newSelectedAnswers,
+        answerAssessment: answerAssessment,
+      };
     }
     case stateActionsLibrary.SET_LOCK: {
       return { ...state, is_locked: true };
